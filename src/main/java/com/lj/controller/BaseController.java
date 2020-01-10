ba257d12c60e6986fdcd89f07fa82452bdcfead5
liangjun <liangjun@grandata.cn>
2020-01-10 14:10:39 +0800

    Initial commit

---
+++ src/main/java/com/lj/controller/BaseController.java
@@ -0,0 +1,23 @@
+package com.lj.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/***
+ * @ClassName : BaseController
+ * @Description : 
+ * @Auther : liangjun
+ * @Date : 2019/8/21 14:16
+ * @version : V1.0
+ */
+public abstract class BaseController {
+    /**
+     * @Author liangjun
+     * @Description ：日志
+     * @Date ：2019/8/21 14:17
+     * @Param ：
+     * @return ：
+     **/
+    protected Logger logger = LoggerFactory.getLogger(this.getClass());
+
+}
